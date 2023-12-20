8748e427d58d85f7ae4b0984dc1e30278e62f14a
rout-horizon <rout.c@horizonprotocol.com>
2023-12-20 19:06:54 +0530

    mainnet config files

--- upgrade-tests/testDebt.js
+++ upgrade-tests/testDebt.js
@@ -8,11 +8,11 @@ const first = async () => {
     const abi = [
         'function balanceOf(address) external view returns (uint)',
     ]
-    const synthetixDebtShare = new ethers.Contract(getContractFromDeployment('SynthetixDebtShare'), abi, provider);
+    // const synthetixDebtShare = new ethers.Contract(getContractFromDeployment('SynthetixDebtShare'), abi, provider);
     // const debtBalance = synthetix.address;
     // const debtBalance = await synthetix.target();
-    // const debtBalance = await synthetix.debtBalanceOf('0xe51738ef83a47aeff4bb4f0b426385a81b543ef6', zUSD)
-    const debtBalance = await synthetixDebtShare.balanceOf('0xe51738ef83a47aeff4bb4f0b426385a81b543ef6')
+    const debtBalance = await synthetix.debtBalanceOf('0xe51738ef83a47aeff4bb4f0b426385a81b543ef6', zUSD)
+    // const debtBalance = await synthetixDebtShare.balanceOf('0xe51738ef83a47aeff4bb4f0b426385a81b543ef6')
     // console.log("debtBlance", synthetix);
     console.log("debtBlance", debtBalance.toString());
 }
